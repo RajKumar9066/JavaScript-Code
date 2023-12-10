984df1d2dca5f300643768fd46844eca353e4b0a
Raj Kumar <90206180+RajKumar9066@users.noreply.github.com>
2023-12-10 15:54:42 +0000

    datatypes done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+
+let name = "Raj"
+let age = 20
+let userLogin = false;
+let state;
+
+
+ //types of variable -:
+
+//  string
+//  number -: 2 to power 53
+//  boolean
+//  bigint
+// null
+// undefined
+// symbol -: unique
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
