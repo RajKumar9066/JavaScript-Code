22227ebeb164a23ee5a63ee07813802a8a2089b8
Raj Kumar <90206180+RajKumar9066@users.noreply.github.com>
2023-12-13 12:35:32 +0000

    Numbers and Maths

---
+++ 01_Basics/06 _nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.9235
+
+console.log(otherNumber.toPrecision(4));
+
+const hundered = 100000
+console.log(hundered.toLocaleString('en-IN'));
+
+//********* Maths In JS ***********/
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.min(5,8,6));
+console.log(Math.max(8,5,9));
+
+console.log(Math.random());
+console.log(Math.random()*10)+1;
+console.log((Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
